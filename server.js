16e23adbfb72f2752448e66b8335622418c4c302
mrSerious <tersoo.atsen@outlook.com>
2018-08-12 14:31:46 +0100

    Finish Server Config

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import apiVersion1 from './routes/routes-api1';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use('/api/v1', apiVersion1);
+
+app.listen(port);
+
+export default app;

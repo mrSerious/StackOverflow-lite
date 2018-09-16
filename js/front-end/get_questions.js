aae8f10d2a6d8a34b5667a16700890a0851e0c42
mrSerious <tersoo.atsen@outlook.com>
2018-09-16 16:50:14 +0100

    ft(update-answer-frontend): consumes the update answer endpoint - complete update answer js logic - fix update answer query [Finishes #160359011]

--- js/front-end/get_questions.js
+++ js/front-end/get_questions.js
@@ -47,7 +47,9 @@ const getAllQuestions = () => {
                 </span>
               </div>
               <h2 class="question-title">
-                <a href="question_details.html" class="question_link" data-id="${array[i].id}">${array[i].title}</a>
+                <a href="question_details.html" class="question_link" data-id="${array[i].id}">
+                  ${array[i].title}
+                </a>
               </h2>
               <div class="tags">
                 <a href="" class="post-tag" title="" rel="tag">java</a>
--- js/front-end/update_answer.js
+++ js/front-end/update_answer.js
@@ -1,5 +1,45 @@
 const questionId = localStorage.getItem('question-id');
 const answerId = localStorage.getItem('answer-id');
-const URL = `https://stack-overflow-lite-app.herokuapp.com/api/v1/questions/${questionId}/answers/${answerId}`;
+const answer = localStorage.getItem('answer');
+const form = document.getElementById('edit_answer_form');
+const accepted = document.getElementsByName('accept_answer');
+const url = `https://stack-overflow-lite-app.herokuapp.com/api/v1/questions/${questionId}/answers/${answerId}`;
 
-console.log(questionId, answerId, URL);
+document.getElementById('original_answer').innerHTML = answer;
+
+const upDateAnswer = (event) => {
+  event.preventDefault();
+  const content = document.getElementById('answer_update');
+  const token = localStorage.getItem('token');
+  let myUpdate;
+
+  if (accepted[0].checked) {
+    myUpdate = { isaccepted: true };
+    console.log('accepted answer selected');
+  } else if (content) {
+    myUpdate = { content: content.value };
+  }
+
+  fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify(myUpdate),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token
+    }
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new TypeError(response.statusText);
+      }
+      return response.json();
+    })
+    .then((myJson) => {
+      console.log(myJson);
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+form.addEventListener('submit', upDateAnswer, false);

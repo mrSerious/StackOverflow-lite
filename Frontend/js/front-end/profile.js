bed226de9e0792b736f41b14a557c9eb523867c1
mrSerious <tersoo.atsen@outlook.com>
2018-09-23 16:51:42 +0100

    ft(user-profile-frontend): consume the user profile endpoint - add profile fetch logic - fix login fetch logic [Finishes #160358197]

--- Frontend/js/front-end/profile.js
+++ Frontend/js/front-end/profile.js
@@ -1,3 +1,22 @@
 const id = localStorage.getItem('user-id');
 const url = `https://stack-overflow-lite-app.herokuapp.com/api/v1/users/${id}`;
-// fetch(url, )
+const token = localStorage.getItem('token');
+
+fetch(url, {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: token
+  }
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new TypeError(response.statusText);
+    }
+    return response.json();
+  })
+  .then((jsonResponse) => {
+    console.log(jsonResponse);
+  })
+  .catch((error) => {
+    throw error;
+  });
